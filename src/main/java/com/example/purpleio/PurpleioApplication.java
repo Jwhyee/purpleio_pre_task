1b46a88e2d1c8ff4b1cdd9d14bc8a077ebe5179f
Jwhyee <kawnsdud@naver.com>
2023-02-05 01:53:29 +0900

    project setting

---
+++ src/main/java/com/example/purpleio/PurpleioApplication.java
@@ -0,0 +1,13 @@
+package com.example.purpleio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurpleioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurpleioApplication.class, args);
+	}
+
+}
