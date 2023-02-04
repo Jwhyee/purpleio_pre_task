fd86d2d80c330976c0cf46c041092efdc3311a3b
Jwhyee <kawnsdud@naver.com>
2023-02-05 03:05:11 +0900

    feat : Search url logging

---
+++ src/main/java/com/example/purpleio/EmbedRestController.java
@@ -0,0 +1,20 @@
+package com.example.purpleio;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@RestController
+@Slf4j
+public class EmbedRestController {
+    @GetMapping("/search")
+    public boolean doSearch(HttpServletRequest rq) {
+        log.info("url={}", rq.getParameter("url"));
+        return true;
+    }
+}
